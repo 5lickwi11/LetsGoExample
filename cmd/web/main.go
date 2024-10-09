165cb55924c8d23336fae78729e31916a437095f
5lickwi11 <trapinifamily@gmail.com>
2024-10-08 21:37:51 -0500

    Update Logging
    Go from printing to the terminal to slogging instead

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-    "flag" // New import
-    "log"
+    "flag"
+    "log/slog"
     "net/http"
+    "os"
 )
 
 func main() {
-    // Define a new command-line flag with the name 'addr', a default value of ":4000"
-    // and some short help text explaining what the flag controls. The value of the
-    // flag will be stored in the addr variable at runtime.
     addr := flag.String("addr", ":4000", "HTTP network address")
-
-    // Importantly, we use the flag.Parse() function to parse the command-line flag.
-    // This reads in the command-line flag value and assigns it to the addr
-    // variable. You need to call this *before* you use the addr variable
-    // otherwise it will always contain the default value of ":4000". If any errors are
-    // encountered during parsing the application will be terminated.
     flag.Parse()
+
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
     mux := http.NewServeMux()
 
@@ -29,14 +23,10 @@ func main() {
     mux.HandleFunc("GET /snippet/create", snippetCreate)
     mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    // The value returned from the flag.String() function is a pointer to the flag
-    // value, not the value itself. So in this code, that means the addr variable
-    // is actually a pointer, and we need to dereference it (i.e. prefix it with
-    // the * symbol) before using it. Note that we're using the log.Printf()
-    // function to interpolate the address with the log message.
-    log.Printf("starting server on %s", *addr)
+    // the old way of printing to terminal: log.Printf("starting server on %s", *addr)
+    logger.Info("starting server", "addr", *addr)
 
-    // And we pass the dereferenced addr pointer to http.ListenAndServe() too.
     err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+    logger.Error(err.Error())
+    os.Exit(1)
 }
